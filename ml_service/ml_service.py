2b5142f4b15a11ca246cb37141f3c65b91b36a8a
Yash Patel <156788298+TheOrcusss@users.noreply.github.com>
2025-11-03 01:11:23 +0530

    pleassss

--- ml_service/ml_service.py
+++ ml_service/ml_service.py
@@ -80,4 +80,5 @@ def recommend_mood_api():
 
 if __name__ == '__main__':
     load_models()
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
